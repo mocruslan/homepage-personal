0e750162f183bfa40227fbb60ea82f4ebf8aa79b
ruslan <mochulskyy.ruslan@gmail.com>
2023-08-15 11:45:12 +0200

    3-implement-navbar
    - Added Header with NavBar component
    - Added Background component

---
+++ components/Background.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Background = () => {
+    return (
+        <>
+            <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
+            <div className="bg-[#dbd7fb] absolute top-[-6rem] left-[-35rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl-left[-5rem]"/>
+        </>
+    );
+}
