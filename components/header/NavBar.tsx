4682d2db108bd5e9cffa46a3485d257e8a214595
ruslan <mochulskyy.ruslan@gmail.com>
2023-08-17 19:40:36 +0200

    1-implement-main-page-content-in-landing-page
    - Updated code styling in NavBar.tsx

--- components/header/NavBar.tsx
+++ components/header/NavBar.tsx
@@ -9,9 +9,9 @@ import {navLinks} from "@/lib/data";
 export const NavBar = () => {
     return (
             <motion.div
-                className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
                 initial={{y: -100, x: "-50%", opacity: 0}}
                 animate={{y: 0, x: "-50%", opacity: 1}}
+                className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
             >
                 <nav className="flex fixed left-1/2 h-12 -translate-x-1/2 py-2  sm:h-[initial] sm:py-0">
                     <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
@@ -35,8 +35,8 @@ const NavItem = (props: NavItemProps) => {
     return (
         <li className="h-3/4 flex items-center justify-center">
             <Link
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
                 href={props.href}
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
             >
                 {props.name}
             </Link>
