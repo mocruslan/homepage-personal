636bc4a45270f367f73c02995ce917f923359122
ruslan <mochulskyy.ruslan@gmail.com>
2023-08-19 15:23:31 +0200

    6-implement-basic-design-in-the-about-page
    - Updated navbar and remove some navbar elements

--- components/header/NavBar.tsx
+++ components/header/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import {motion} from "framer-motion";
 import {navLinks} from "@/lib/data";
