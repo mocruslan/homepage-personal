502c536c5be6fe490ce9b0a308f231ee12d02cca
ruslan <mochulskyy.ruslan@gmail.com>
2023-08-19 13:47:44 +0200

    1-implement-main-page-content-in-landing-page
    - Added actions to each button
    - Removed AnchorButton.tsx since it's not useful right now

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,28 @@ import {BiArrowToRight, BiDownload} from "react-icons/bi";
 import {AnchorButtonAnimated} from "@/components/shared/AnchorButtonAnimated";
 import {BsLinkedin} from "react-icons/bs";
 import {FaGithubSquare} from "react-icons/fa";
+import {LinkButton} from "@/components/shared/LinkButton";
+import {AnchorButton} from "@/components/shared/AnchorButton";
 
 
 export default function Home() {
     return (
         <main className="container mx-auto">
-            <section className="flex flex-col lg:flex-row items-center justify-center">
+            <section className="flex flex-col items-center justify-center lg:flex-row">
                 <div className="flex items-center justify-center">
                     <Image
                         src={"/img/profile-picture.png"}
                         alt={"Ruslan's profile picture"}
-                        width={200}
-                        height={200}
+                        width={300}
+                        height={300}
                         priority={true}
                         quality={90}
                         className="h-80 w-80 object-cover sm:h-[25rem] sm:w-[25rem] lg:h-[30rem] lg:w-[30rem]"
                     />
                 </div>
 
-                <div className="flex flex-col items-start justify-start mx-10 sm:mx-20">
-                    <span className="flex flex-col  sm:max-w-md">
+                <div className="flex flex-col items-start justify-start mx-10 sm:mx-20 gap-6">
+                    <span className="flex flex-col sm:max-w-md">
                         <h1 className="text-left text-2xl sm:text-4xl font-bold leading-snug align-baseline">
                             Turning Vision into <br/> reality with code <br/> and design.
                         </h1>
@@ -33,31 +35,34 @@ export default function Home() {
                         </span>
                     </span>
 
-                    <div className="flex items-start justify-start gap-2 text-sm">
-                        <LinkButtonAnimated
+                    <div className="flex items-center justify-center gap-3 text-[0.6rem] sm:text-sm">
+                        <LinkButton
                             href={"/"}
-                            className="group flex items-center gap-2 bg-gray-900 text-white px-7 py-2 rounded-full outline-none  hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                            className="group flex items-center gap-2 bg-gray-900 text-white px-7 py-2 rounded-full whitespace-nowrap border border-black/10  cursor-pointer outline-none  hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
                         >
-                            Contact me here {" "} <BiArrowToRight className="opacity-70 group-hover:translate-x-1 transition"/>
-                        </LinkButtonAnimated>
+                            Contact me here {" "} <BiArrowToRight className="opacity-70 group-hover:translate-x-2 transition"/>
+                        </LinkButton>
 
-                        <AnchorButtonAnimated
-                            className="group flex items-center gap-2 bg-white px-7 py-2 rounded-full outline-none hover:scale-110 active:scale-105 transition"
+                        <a
+                            href={"/file/CV.pdf"} download
+                            className="group flex items-center gap-2 bg-white px-7 py-2 rounded-full whitespace-nowrap border border-black/10 cursor-pointer outline-none hover:scale-110 hover:text-gray-950 active:scale-105 transition "
                         >
                             Download CV {" "} <BiDownload className="opacity-70 group-hover:translate-y-1 transition"/>
-                        </AnchorButtonAnimated>
+                        </a>
 
-                        <AnchorButtonAnimated
-                            className="group flex items-center gap-2 grow-0 bg-white text-gray-700 text-[1.2rem] p-2 rounded-full outline-none hover:scale-110 active:scale-105 transition"
+                        <a
+                            href={"https://www.linkedin.com/in/ruslan-mochulskyy/"} target={"_blank"}
+                            className="group flex items-center gap-2 grow-0 bg-white text-gray-700 text-[1.2rem] p-2 rounded-full border border-black/10  cursor-pointer outline-none hover:scale-110 hover:text-gray-950 active:scale-105 transition"
                         >
                             <BsLinkedin/>
-                        </AnchorButtonAnimated>
+                        </a>
 
-                        <AnchorButtonAnimated
-                            className="group flex items-center gap-2 grow-0 bg-white text-gray-700 text-[1.2rem] p-2 rounded-full outline-none hover:scale-110 active:scale-105 transition"
+                        <a
+                            href={"https://github.com/mocruslan"} target={"_blank"}
+                            className="group flex items-center gap-2 grow-0 bg-white text-gray-700 text-[1.2rem] p-2 rounded-full border border-black/10  cursor-pointer outline-none hover:scale-110 hover:text-gray-950 active:scale-105 transition"
                         >
                             <FaGithubSquare/>
-                        </AnchorButtonAnimated>
+                        </a>
                     </div>
                 </div>
             </section>
