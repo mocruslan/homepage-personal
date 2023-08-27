5b407caecb5a654270a17ba173c61229ecb42827
ruslan <mochulskyy.ruslan@gmail.com>
2023-08-27 23:02:41 +0200

    6-implement-basic-design-in-the-about-page
    - Updated packages

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,9 @@
+import {FaAws, FaC, FaCss3, FaGolang, FaHtml5, FaJava, FaJs, FaNodeJs, FaPhp, FaPython} from "react-icons/fa6";
+import {FaReact} from "react-icons/fa";
+import {BiLogoMongodb, BiLogoTypescript} from "react-icons/bi";
+import {SiAmazondynamodb, SiMariadb} from "react-icons/si";
+import {TbBrandNextjs} from "react-icons/tb";
+
 export const navLinks = [
     {
         name: 'Home',
@@ -14,5 +20,73 @@ export const navLinks = [
     {
         name: 'Contact',
         href: '/experience',
-    }
+    },
+] as const;
+
+
+export const skills = [
+    {
+        name: 'C',
+        icon: FaC,
+    },
+    {
+        name: 'PHP',
+        icon: FaPhp,
+    },
+    {
+        name: 'JavaScript',
+        icon: FaJs,
+    },
+    {
+        name: 'TypeScript',
+        icon: BiLogoTypescript,
+    },
+    {
+        name: 'Golang',
+        icon: FaGolang,
+    },
+    {
+        name: 'Python',
+        icon: FaPython,
+    },
+    {
+        name: 'Java',
+        icon: FaJava,
+    },
+    {
+        name: 'React',
+        icon: FaReact,
+    },
+    {
+        name: 'Next.js',
+        icon: TbBrandNextjs,
+    },
+    {
+        name: 'AWS',
+        icon: FaAws,
+    },
+    {
+        name: 'HTML',
+        icon: FaHtml5,
+    },
+    {
+        name: 'CSS',
+        icon: FaCss3,
+    },
+    {
+        name: 'MongoDB',
+        icon: BiLogoMongodb,
+    },
+    {
+        name: 'MariaDB',
+        icon: SiMariadb,
+    },
+    {
+        name: 'DynamoDB',
+        icon: SiAmazondynamodb,
+    },
+    {
+        name: 'Node.js',
+        icon: FaNodeJs,
+    },
 ] as const;
