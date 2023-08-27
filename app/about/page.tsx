087c01c36c153082d44b1de6967b1848b59957a3
ruslan <mochulskyy.ruslan@gmail.com>
2023-08-27 23:02:27 +0200

    6-implement-basic-design-in-the-about-page
    - Added Skills section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,17 @@
 import {LottieAutoplay} from "@/components/shared/LottieAutoplay";
+import {SectionDivider} from "@/components/shared/SectionDivider";
+import {Skills} from "@/components/page/about/Skills";
 
 const About = () => {
     return (
-        <main className="container mx-auto px-6">
+        <main className="container mx-auto px-6 flex flex-col items-center justify-center gap-10">
             <section className="flex flex-col-reverse items-center justify-center md:flex-row">
-                <span className="flex flex-col gap-3 w-full px-6 text-left sm:w-[25rem]">
-                    <h2 className="text-2xl font-bold align-baseline">
+                <span className="flex flex-col gap-3 w-full px-6 sm:w-[25rem]">
+                    <h2 className="text-2xl text-center md:text-left font-bold align-baseline">
                         Biography
                     </h2>
 
-                     <article className="text-gray-950 font-extralight">
+                     <article className="text-gray-950 text-left font-extralight">
                          Fugiat minim irure mollit amet non dolore amet ex nulla enim reprehenderit nisi. Non dolor deserunt ea proident anim. Exercitation veniam aliquip occaecat esse pariatur culpa exercitation nulla fugiat aliquip Lorem ad Lorem. Pariatur fugiat proident ex in. Proident aliqua consequat aliquip cillum nostrud irure. Irure duis aliqua minim.
                     </article>
                 </span>
@@ -18,6 +20,11 @@ const About = () => {
                         <LottieAutoplay src={"https://lottie.host/5be5a792-af38-4e4d-ac30-1fd4cd04ee66/iAZtJErLKq.json"}/>
                 </div>
             </section>
+
+            <SectionDivider/>
+
+            <Skills/>
+
         </main>
     );
 }
