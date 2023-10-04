fe4d5b6d7862ef50700a5f26f083b98e2d30d8e1
ruslan <mochulskyy.ruslan@gmail.com>
2023-10-04 23:14:47 +0200

    14-add-email-functionality
    - Fixed contact me button redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const Home = () => {
 
                     <div className="flex items-center justify-center gap-3 text-[0.6rem] sm:text-sm">
                         <LinkButton
-                            href={"/"}
+                            href={"/contact"}
                             className="group flex items-center gap-2 bg-gray-900 text-white px-7 py-2 rounded-full whitespace-nowrap border border-black/10  cursor-pointer outline-none  hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
                         >
                             Contact me here {" "} <BiArrowToRight className="opacity-70 group-hover:translate-x-2 transition"/>
