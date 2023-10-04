e2ae335e321276b5d20ae805b6f2c41efc1e441e
ruslan <mochulskyy.ruslan@gmail.com>
2023-10-04 22:58:37 +0200

    14-add-email-functionality
    - Removed unused API_KEY in AbstractMailAPI.ts

--- src/lib/mail/abstract/AbstractMailAPI.ts
+++ src/lib/mail/abstract/AbstractMailAPI.ts
@@ -1,7 +1,5 @@
 import {IMail} from "@/src/lib/mail/interface/IMail";
 
-const RESEND_API_KEY = process.env.RESEND_API_KEY;
-
 export abstract class AbstractMailAPI implements IMail {
     protected abstract getClient(): any;
 
